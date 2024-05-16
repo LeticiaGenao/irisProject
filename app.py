62850c7f13f3438f698c3dfe422f7d7d9ab786d1
LeticiaG <leticiagenao403@gmail.com>
2024-05-16 15:25:49 -0400

    Initial commit of project files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        Sepal_Length = float(request.form['sepal_length'])
+        Sepal_Width = float(request.form['sepal_width'])
+        Petal_Length = float(request.form['petal_length'])
+        Petal_Width = float(request.form['petal_width'])
+        prediction = model.predict(np.array([[Sepal_Length, Sepal_Width, Petal_Length, Petal_Width]]))
+        species = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}[prediction[0]]  # Update according to your label encoding
+        return render_template('index.html', prediction=f'Species predicted: {species}')
+    except Exception as e:
+        return render_template('index.html', prediction=f'Error: {str(e)}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
